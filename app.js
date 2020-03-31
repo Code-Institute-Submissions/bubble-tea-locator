f3240fbc3920dcc3cbff53ca140433de8fbdccc0
Melville <melville1992@gmail.com>
2020-03-31 14:19:18 +0000

    add in json file

--- app.js
+++ app.js
@@ -2,17 +2,7 @@
 $(function() {
 
     let map = createMap("map", [1.3521, 103.8198], 13);
-    console.log("testingtiger.geojson")
-
-    axios.get("testingtiger.geojson").then(function(b){
-        let location = b.features[x].geometry.coordinates
-        console.log(location)
-        for (let l of location){
-            let place = L.marker([l[1],l[0]]);
-            place.addTo(map);
-        }
-        
-    })
-  
+    
+    let singaporeMarker = L.marker([1.29321,103.85105]).addTo(map);
 })
 
