52a041ab6929fb62ba5ec2e8fd0ab62968fb631b
Melville <melville1992@gmail.com>
2020-04-01 07:23:52 +0000

    added table into app.js

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ $(function() {
 
     let map = createMap("map", [1.3521, 103.8198], 13);
     
-    axios.get("testingtiger.json").then(function(response){
-        
+    axios.get("testingtiger.json",{
+        params:{
+            "name":"name",
+            "address":"address",
+            "hour":"hour",
+        }
+    }).then(function(response){
         
         
         // let location = response.data.features[0].geometry.coordinates;
@@ -14,7 +19,12 @@ $(function() {
             let lng = l.geometry.coordinates[0];
             let m = L.marker([lat,lng]);
             console.log(l.geometry.coordinates);
-            m.addTo(map);
+            m.bindPopup(`<table><tr><th>Name:</th><td>${l.properties.name}</td></tr>
+                        <tr><th>Address:</th><td>${l.properties.address}</td></tr>
+                        <tr><th>Opening-hours:</th><td>${l.properties.hour}</td></tr></table>`);
+            m.addTo(map)
+        
+
         }
         
     })
