40b0225a528845d2943edf91d9367b7ddd68d770
Melville <melville1992@gmail.com>
2020-04-01 03:41:45 +0000

    added marker on the map

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ $(function() {
 
     let map = createMap("map", [1.3521, 103.8198], 13);
     
-    let singaporeMarker = L.marker([1.29321,103.85105]).addTo(map);
+    axios.get("testingtiger.json").then(function(response){
+        
+        
+        
+        // let location = response.data.features[0].geometry.coordinates;
+        let locations = response.data.features;
+        for(l of locations){
+            let lat = l.geometry.coordinates[1];
+            let lng = l.geometry.coordinates[0];
+            let m = L.marker([lat,lng]);
+            console.log(l.geometry.coordinates);
+            m.addTo(map);
+        }
+        
+    })
+
+    
 })
 
