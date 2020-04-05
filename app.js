5f578f82066cab0e8a0922d8dd12a9559be0c0c5
Melville <melville1992@gmail.com>
2020-04-05 10:24:39 +0000

    Added select dropdown box

--- app.js
+++ app.js
@@ -2,32 +2,6 @@
 $(function() {
 
     let map = createMap("map", [1.3521, 103.8198], 13);
-    
-    // axios.get("testingtiger.geojson",{
-    //     params:{
-    //         "name":"name",
-    //         "address":"address",
-    //         "hour":"hour",
-    //     }
-    // }).then(function(response){
-        
-        
-    //     // let location = response.data.features[0].geometry.coordinates;
-    //     let locations = response.data.features;
-    //     for(l of locations){
-    //         let lat = l.geometry.coordinates[1];
-    //         let lng = l.geometry.coordinates[0];
-    //         let m = L.marker([lat,lng]);
-    //         console.log(l.geometry.coordinates);
-    //         m.bindPopup(`<table><tr><th>Name:</th><td>${l.properties.name}</td></tr>
-    //                     <tr><th>Address:</th><td>${l.properties.address}</td></tr>
-    //                     <tr><th>Opening-hours:</th><td>${l.properties.hour}</td></tr></table>`);
-    //         m.addTo(map)
-        
-
-    //     }
-        
-    // })
 
     let promise = [axios.get('testingtiger.geojson'),axios.get('xinfutang.geojson')];
     axios.all(promise).then(axios.spread(function(tiger,xinfutang){
@@ -37,6 +11,9 @@ $(function() {
              let lat = t.geometry.coordinates[1];
              let lng = t.geometry.coordinates[0];
              let marker = L.marker([lat,lng]);
+             marker.bindPopup(`<table><tr><th>Name:</th><td>${t.properties.name}</td></tr>
+                                <tr><th>Address:</th><td>${t.properties.address}</td></tr>
+                                <tr><th>Opening-hours:</th><td>${t.properties.hour}</td></tr></table>`);
              tigerGroup.addLayer(marker);
         }
         
@@ -45,11 +22,22 @@ $(function() {
              let lat = x.geometry.coordinates[1];
              let lng = x.geometry.coordinates[0];
              let marker = L.marker([lat,lng]);
+             marker.bindPopup(`<table><tr><th>Name:</th><td>${x.properties.name}</td></tr>
+                                <tr><th>Address:</th><td>${x.properties.address}</td></tr>
+                                <tr><th>Opening-hours:</th><td>${x.properties.hour}</td></tr></table>`);
              xinfutangGroup.addLayer(marker);
         }
 
         map.addLayer(tigerGroup);
         map.addLayer(xinfutangGroup);
+
+        let bbtLayers = {
+            'tiger':'tigerGroup',
+            'xinfutang':'xinfutangGroup'
+        }
+
+        L.control.layers(bbtLayers).addTo(map);
+        
 
 
     }))
